7d88782a46c88592125aa6d20c5e92c431e4dac3
Jonathan Olbes <olbes.es@gmail.com>
2026-01-25 19:16:49 +0100

    Change alias on file to connect DB

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -1,5 +1,5 @@
-import { connectDB } from "@/lib/db";
-import Lead from "@/lib/models/Lead";
+import { connectDB } from "@/app/lib/db";
+import Lead from "@/app/lib/models/Lead";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
